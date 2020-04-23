6591e2f9638421e0ec27046b9c9cfd3fd421cb7e
Thib <thibault.cpt@eduge.ch>
2020-04-23 09:51:54 +0200

    Màj js

--- publicPost.php
+++ publicPost.php
@@ -781,9 +781,9 @@
 <!-- jQuery -->
 <script src="./JS/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="./js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
+<script src="./js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="../../dist/js/demo.js"></script>
 <!-- Summernote -->
