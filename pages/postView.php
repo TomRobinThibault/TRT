32626a84d97900ce3c1b34a95b18cfd1d6fcf544
Robin Laborde <robin.lbrd@eduge.ch>
2020-05-07 10:24:02 +0200

    Ajout du style de la list post

--- pages/postView.php
+++ pages/postView.php
@@ -7,6 +7,81 @@
 
 <head>
     <meta charset="utf-8">
+    <title>Listes des Posts</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Tempusdominus Bbootstrap 4 -->
+    <link rel="stylesheet" href="../plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- JQVMap -->
+    <link rel="stylesheet" href="../plugins/jqvmap/jqvmap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../css/adminlte.css">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="../plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="../plugins/daterangepicker/daterangepicker.css">
+    <!-- summernote -->
+    <link rel="stylesheet" href="../plugins/summernote/summernote-bs4.css">
+    <!-- Google Font: Source Sans Pro -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+
+        <!-- navigations -->
+        <?php include("../inc/nav.inc.php"); ?>
+        <?php include("../inc/sidebar.inc.php"); ?>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0 text-dark">Liste des posts</h1>
+                        </div>
+                        <table class="table">
+                            <tr>
+                                <td>Numéro</td>
+                                <td>Auteur</td>
+                                <td>Titre</td>
+                                <td>Description</td>
+                                <td>Date de création</td>
+                                <td>Dernière modification</td>
+                            </tr>
+                            <tr>
+                                <td>0026</td>
+                                <td>LABORDE Robin</td>
+                                <td>Qu'est ce qui est jaune et qui attend</td>
+                                <td>tel est la question</td>
+                                <td>07 mai 2020</td>
+                                <td>07 mai 2020</td>
+                            </tr>
+                        </table>
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.content-header -->
+        </div>
+        <!-- /.content -->
+    </div>
+</body>
+
+<!-- /.content-wrapper -->
+<footer class="main-footer">
+    <strong>Copyright &copy; 2020-2020 Adrivet Tom, Capt Thibault, Laborde Robin </strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 1.0.1
+    </div>
+</footer>
 
 </html>
