dc725ddd39daeef06604b6781213ea33a3fe3bfd
CanardConfit <canardconfit.developpement@gmail.com>
2020-05-14 11:23:10 +0200

    Create User API Fonctions

--- sql/Server.php
+++ sql/Server.php
@@ -2,9 +2,9 @@
 class Server
 {
     private $host = "127.0.0.1";
-    private $database = "quiquiz";
-    private $user = "ecole_quiquiz";
-    private $password = "dh9fbH4ujccXfV4Q";
+    private $database = "ecole_trt";
+    private $user = "root";
+    private $password = "";
 
     private $connection;
 
@@ -16,38 +16,44 @@ class Server
     function __destruct() {
         $this->connection = null;
     }
-    function updateUser($idJoueur, $champ, $valeur) {
-        $stmt = $this->connection->prepare("UPDATE joueur SET :champ=:valeur WHERE idJoueur=:idJoueur;");
-        $stmt->bindParam("idJoueur", $idJoueur, PDO::PARAM_INT);
-        $stmt->bindParam("champ", $champ, PDO::PARAM_STR);
-        $stmt->bindParam("valeur", $valeur, PDO::PARAM_STR);
+
+    /*
+     *
+     *
+     * Fonction pour gérer les utilisateurs
+     *
+     *
+     */
+    function createUser($pseudo, $hash) {
+        $stmt = $this->connection->prepare("INSERT INTO utilisateur(pseudoUser, hashUser, dateCreation, dateLastUtilisation) VALUES (:pseudo,:hash,:curDate,:lastDate);");
+
+        $dateCreation = date("d/m/y H:i:s");
+
+        $stmt->bindParam("pseudo", $pseudo, PDO::PARAM_STR);
+        $stmt->bindParam("hash", $hash, PDO::PARAM_STR);
+        $stmt->bindParam("curDate", $dateCreation, PDO::PARAM_STR);
+        $stmt->bindParam("lastDate", $dateCreation, PDO::PARAM_STR);
         return $stmt->execute();
     }
-    function createScore($idJoueur, $idQuiz, $score) {
-        $stmt = $this->connection->prepare("INSERT INTO avoir(idJoueur, idQuiz, score) VALUES (:idJoueur,:idQuiz,:score);");
-        $stmt->bindParam("idJoueur", $idJoueur, PDO::PARAM_INT);
-        $stmt->bindParam("idQuiz", $idQuiz, PDO::PARAM_INT);
-        $stmt->bindParam("score", $score, PDO::PARAM_INT);
+    function updateUserUse($pseudo) {
+        $stmt = $this->connection->prepare("UPDATE utilisateur SET dateLastUtilisation = :lastDate WHERE pseudoUser = :pseudo;");
+
+        $date = date("d/m/y H:i:s");
+
+        $stmt->bindParam("pseudo", $pseudo, PDO::PARAM_STR);
+        $stmt->bindParam("lastDate", $date, PDO::PARAM_STR);
         return $stmt->execute();
     }
-    function asScore($idJoueur, $idQuiz)
-    {
-        $stmt = $this->connection->prepare("SELECT score FROM avoir WHERE idJoueur=:idJoueur AND idQuiz=:idQuiz;");
-        $stmt->bindParam("idJoueur", $idJoueur, PDO::PARAM_INT);
-        $stmt->bindParam("idQuiz", $idQuiz, PDO::PARAM_INT);
+    function getUserByPseudo($pseudo) {
+        $stmt = $this->connection->prepare("SELECT * FROM utilisateur WHERE pseudoUser=:pseudo;");
+        $stmt->bindParam("pseudo", $pseudo, PDO::PARAM_STR);
         $stmt->execute();
-        return $stmt->rowCount() >= 1;
+        return $stmt->fetchAll(PDO::FETCH_OBJ)[0];
     }
-    function getJoueurIdFromMail($mail) {
-        $stmt = $this->connection->prepare("SELECT idJoueur FROM joueur WHERE mailJoueur=:mail;");
-        $stmt->bindParam("mail", $mail, PDO::PARAM_STR);
+    function testPasswordUser($pseudo, $hash) {
+        $stmt = $this->connection->prepare("SELECT hashUser FROM utilisateur WHERE pseudoUser = :pseudo;");
+        $stmt->bindParam("pseudo", $pseudo, PDO::PARAM_INT);
         $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_OBJ);
-    }
-    function asGoodPassword($idJoueur, $hash) {
-        $stmt = $this->connection->prepare("SELECT hashJoueur FROM joueur WHERE idJoueur=:idJoueur;");
-        $stmt->bindParam("idJoueur", $idJoueur, PDO::PARAM_INT);
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_OBJ)[0]->hashJoueur == $hash;
+        return $stmt->fetchAll(PDO::FETCH_OBJ)[0]->hashUser == $hash;
     }
 }
